74e853eeecd1288afb0d73c7fcc43ec6cf9a8da4
samuel-peter-chowdhury <samuel.peter.chowdhury@gmail.com>
2024-10-09 17:01:12 -0400

    add 35 pokes toolbar

--- search.js
+++ search.js
@@ -307,7 +307,33 @@ const filterList = [
 		return 'Error: not found';
 	};
 	Search.prototype.renderPokemonSortRow = function () {
-		var buf = '<li class="result"><div class="sortrow">';
+		var buf = '<li class="result"><div class="sortrow">'
+		buf += '<select id="35-pokes-month" name="35-pokes-month" class="button">'
+		buf += '<option value="1">Jan</option>'
+		buf += '<option value="2">Feb</option>'
+		buf += '<option value="3">Mar</option>'
+		buf += '<option value="4">Apr</option>'
+		buf += '<option value="5">May</option>'
+		buf += '<option value="6">Jun</option>'
+		buf += '<option value="7">Jul</option>'
+		buf += '<option value="8">Aug</option>'
+		buf += '<option value="9">Sep</option>'
+		buf += '<option value="10">Oct</option>'
+		buf += '<option value="11">Nov</option>'
+		buf += '<option value="12">Dec</option>'
+		buf += '</select>'
+		buf += '<select id="35-pokes-year" name="35-pokes-year" class="button">'
+		buf += '<option value="2023">2023</option>'
+		buf += '<option value="2024">2024</option>'
+		buf += '<option value="2025">2025</option>'
+		buf += '<option value="2026">2026</option>'
+		buf += '<option value="2027">2027</option>'
+		buf += '<option value="2028">2028</option>'
+		buf += '<option value="2029">2029</option>'
+		buf += '<option value="2030">2030</option>'
+		buf += '</select>'
+		buf += '</div></li>';
+		buf += '<li class="result"><div class="sortrow">';
 		buf += '<button class="sortcol numsortcol' + (!this.sortCol ? ' cur' : '') + '">' + (!this.sortCol ? 'Sort: ' : this.engine.firstPokemonColumn) + '</button>';
 		buf += '<button class="sortcol pnamesortcol' + (this.sortCol === 'name' ? ' cur' : '') + '" data-sort="name">Name</button>';
 		buf += '<button class="sortcol typesortcol' + (this.sortCol === 'type' ? ' cur' : '') + '" data-sort="type">Types</button>';
